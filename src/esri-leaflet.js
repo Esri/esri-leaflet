e83f826b9af2985b8c91dd27899ed3055e244885
Patrick Arlt <patrick.arlt@gmail.com>
2013-07-27 17:48:14 -0700

    Add CORS and JSONP AJAX handlers, add more comments

--- src/esri-leaflet.js
+++ src/esri-leaflet.js
@@ -4,12 +4,75 @@ L.esri = {
   AttributionStyles:"line-height:9px; text-overflow:ellipsis; white-space:nowrap;overflow:hidden; display:inline-block;",
   LogoStyles:"position:absolute; top:-38px; right:2px;",
   _callback: {},
-  get: function(url, params, callback){
+};
+
+// Namespace for various support variables we need to track
+L.esri.Support = {
+  // from: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/cors.js#L20
+  CORS: !!(window.XMLHttpRequest && 'withCredentials' in new XMLHttpRequest())
+};
+
+// AJAX handlers for CORS (modern browsers) or JSONP (older browsers)
+L.esri.RequestHandlers = {
+  CORS: function(url, params, callback, context){
+    var httpRequest = new XMLHttpRequest();
+
+    params.f="json";
+
+    httpRequest.onreadystatechange = function(){
+      var response;
+      if (httpRequest.readyState === 4) {
+        try {
+          response = JSON.parse(httpRequest.responseText);
+        } catch(e) {
+          response = {
+            error: "Could not parse response as JSON."
+          };
+        }
+        if(context){
+          callback.call(context, response);
+        } else {
+          callback(response);
+        }
+      }
+    };
+
+    httpRequest.open('GET', url + L.esri.Util.serialize(params), true);
+    httpRequest.send(null);
+  },
+  JSONP: function(url, params, callback, context){
     var callbackId = "c"+(Math.random() * 1e9).toString(36).replace(".", "_");
 
     params.f="json";
     params.callback="L.esri._callback."+callbackId;
 
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url + L.esri.Util.serialize(params);
+    script.id = callbackId;
+
+    L.esri._callback[callbackId] = function(response){
+      if(context){
+        callback.call(context, response);
+      } else {
+        callback(response);
+      }
+      document.body.removeChild(script);
+      delete L.esri._callback[callbackId];
+    };
+
+    document.body.appendChild(script);
+
+  }
+};
+
+// Choose the correct AJAX handler depending on CORS support
+L.esri.get = (L.esri.Support.CORS) ? L.esri.RequestHandlers.CORS : L.esri.RequestHandlers.JSONP;
+
+// General utility namespace
+L.esri.Util = {
+  // quick and dirty serialization
+  serialize: function(params){
     var qs="?";
 
     for(var param in params){
@@ -23,25 +86,10 @@ L.esri = {
       }
     }
 
-    qs = qs.substring(0, qs.length - 1);
+    return qs.substring(0, qs.length - 1);
+  },
 
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url + qs;
-    script.id = callbackId;
-
-    L.esri._callback[callbackId] = function(response){
-      callback(response);
-      document.body.removeChild(script);
-      delete L.esri._callback[callbackId];
-    };
-
-    document.body.appendChild(script);
-
-  }
-};
-
-L.esri.Util = {
+  // index of polyfill, needed for IE 8
   indexOf: function(arr, obj, start){
     start = start || 0;
     if(arr.indexOf){
@@ -52,12 +100,15 @@ L.esri.Util = {
     }
     return -1;
   },
+
+  // convert an extent (ArcGIS) to LatLngBounds (Leaflet)
   extentToBounds: function(extent){
     var southWest = new L.LatLng(extent.xmin, extent.ymin);
     var northEast = new L.LatLng(extent.xmax, extent.ymin);
     return new L.LatLngBounds(southWest, northEast);
   },
 
+  // convert an LatLngBounds (Leaflet) to extent (ArcGIS)
   boundsToExtent: function(bounds) {
     return {
       "xmin": bounds.getSouthWest().lng,
@@ -69,6 +120,8 @@ L.esri.Util = {
       }
     };
   },
+
+  // convert a LatLngBounds (Leaflet) to a Envelope (Terraformer.Rtree)
   boundsToEnvelope: function(bounds){
     var extent = L.esri.Util.boundsToExtent(bounds);
     return {
