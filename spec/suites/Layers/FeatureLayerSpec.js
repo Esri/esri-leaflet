8948f9b14dc3c383d35f9901c99123fe67a32a7d
odoe <odoe@odoe.net>
2013-08-06 12:16:28 -0700

    updated tests, created a sandbox for sinon to restore all spies/stubs, fixed small type in L.esri.Util.extentToBounds that caused incorrect bounds to be defined.

--- spec/suites/Layers/FeatureLayerSpec.js
+++ spec/suites/Layers/FeatureLayerSpec.js
@@ -1,86 +1,95 @@
-/* globals L:true describe:true it:true expect:true*/
+/* globals L, describe, beforeEach, afterEach, it, expect, Terraformer*/
 
-describe('L.esri.FeatureLayer', function() {
-    var map,
-        url = 'http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/stops/FeatureServer/0/';
-    beforeEach(function () {
-        map = L.map(document.createElement('div'));
-        map.setView([37.75,-122.45], 12);
+describe('L.esri.FeatureLayer', function () {
+  var map,
+    sandbox,
+    url = 'http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/stops/FeatureServer/0/';
+  beforeEach(function () {
+    map = L.map(document.createElement('div'));
+    map.setView([37.75, -122.45], 12);
+    sandbox = sinon.sandbox.create();
+  });
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  describe('#constructor', function () {
+    describe('when a FeatureLayer is first created', function () {
+      it('will have an index defined by Terraformer', function () {
+        var rtreeSpy = sandbox.spy(Terraformer, 'RTree'),
+          featLayer = L.esri.featureLayer(url);
+        expect(rtreeSpy.called).to.be.ok();
+        expect(featLayer.index).to.be.ok();
+      });
     });
+  });
 
-    describe('#constructor', function() {
-        describe('when a FeatureLayer is first created', function() {
-            it('will have an index defined by Terraformer', function() {
-                var featLayer = L.esri.featureLayer(url);
-                expect(featLayer.index).to.be.ok();
-            });
-        });
+  describe('#onAdd, #onRemove, #updateFeatures', function () {
+    describe('when a FeatureLayer is added to the map', function () {
+      it('will call its own updateFeatures', function () {
+        var featLayer = L.esri.featureLayer(url),
+          updateFeaturesSpy = sandbox.spy(featLayer, 'updateFeatures');
+        featLayer.onAdd(map);
+        expect(updateFeaturesSpy.calledWith(map)).to.be.ok();
+      });
+
+      it('will have updateFeatures draw the features on the map', function (done) {
+        var featLayer = L.esri.featureLayer(url),
+          onSpy = sandbox.spy(map, 'on');
+        featLayer.onAdd(map);
+        setTimeout(function () {
+          expect(onSpy.calledWith('viewreset moveend')).to.be.ok();
+          done();
+        }, 1000);
+      });
+
+      it('will search the Terraformer index', function (done) {
+        var featLayer = L.esri.featureLayer(url),
+          onSpy = sandbox.spy(map, 'on'),
+          searchStub = sandbox.stub(featLayer.index, 'search').returns({
+            then: function () {
+            }
+          });
+        featLayer.onAdd(map);
+        setTimeout(function () {
+          expect(onSpy.calledWith('viewreset moveend')).to.be.ok();
+          expect(searchStub.calledOnce).to.be.ok();
+          done();
+        }, 1000);
+      });
+
+      it('will query map service for features in envelope', function (done) {
+        var featLayer = L.esri.featureLayer(url),
+          onSpy = sandbox.spy(map, 'on'),
+          searchStub = sandbox.stub(featLayer.index, 'search').returns({
+            then: function () {
+            }
+          }),
+          getSpy = sandbox.spy(L.esri, 'get');
+        featLayer.onAdd(map);
+        setTimeout(function () {
+          expect(onSpy.calledWith('viewreset moveend')).to.be.ok();
+          expect(searchStub.calledOnce).to.be.ok();
+          expect(getSpy.calledOnce).to.be.ok();
+          done();
+        }, 1000);
+      });
     });
-
-    describe('#onAdd, #onRemove, #updateFeatures', function() {
-        describe('when a FeatureLayer is added to the map', function() {
-            it('will call its own updateFeatures', function() {
-                var featLayer = L.esri.featureLayer(url);
-                featLayer.updateFeatures = sinon.spy();
-                featLayer.onAdd(map);
-                expect(featLayer.updateFeatures.called).to.be.ok();
-            });
-
-            it('will have updateFeatures draw the features on the map', function(done) {
-                var featLayer = L.esri.featureLayer(url);
-                map.on = sinon.spy();
-                featLayer.onAdd(map);
-                setTimeout(function() {
-                    expect(map.on.called).to.be.ok();
-                    done();
-                }, 1000);
-            });
-
-            it('will search the Terraformer index', function(done) {
-                var featLayer = L.esri.featureLayer(url);
-                map.on = sinon.spy();
-                featLayer.index.search = sinon.stub().returns({
-                    then: function(){}
-                });
-                featLayer.onAdd(map);
-                setTimeout(function() {
-                    expect(map.on.called).to.be.ok();
-                    expect(featLayer.index.search.called).to.be.ok();
-                    done();
-                }, 1000);
-            });
-
-            it('will query map service for features in envelope', function(done) {
-                var featLayer = L.esri.featureLayer(url);
-                map.on = sinon.spy();
-                featLayer.index.search = sinon.stub().returns({
-                    then: function(){}
-                });
-                L.esri.get = sinon.spy();
-                featLayer.onAdd(map);
-                setTimeout(function() {
-                    expect(map.on.called).to.be.ok();
-                    expect(featLayer.index.search.called).to.be.ok();
-                    expect(L.esri.get.called).to.be.ok();
-                    done();
-                }, 1000);
-            });
-        });
-        describe('when a FeatureLayer is removed from the map', function() {
-            it('will remove associated events from the map', function() {
-                var featLayer = L.esri.featureLayer(url);
-                map.off = sinon.spy();
-                featLayer.onRemove(map);
-                expect(map.off.calledOnce).to.be.ok();
-            });
-        });
+    describe('when a FeatureLayer is removed from the map', function () {
+      it('will remove associated events from the map', function () {
+        var featLayer = L.esri.featureLayer(url),
+          offSpy = sandbox.spy(map, 'off');
+        featLayer.onRemove(map);
+        expect(offSpy.calledOnce).to.be.ok();
+      });
     });
-    describe('#getLayerId', function() {
-        describe('when asked for a layer id', function() {
-            it('will return the FeatureLayer id for layer it is given', function() {
-                var featLayer = L.esri.featureLayer(url);
-                expect(featLayer.getLayerId({ feature: { id: 'LAYERID' } })).to.eql('LAYERID');
-            });
-        });
+  });
+  describe('#getLayerId', function () {
+    describe('when asked for a layer id', function () {
+      it('will return the FeatureLayer id for layer it is given', function () {
+        var featLayer = L.esri.featureLayer(url);
+        expect(featLayer.getLayerId({ feature: { id: 'LAYERID' } })).to.eql('LAYERID');
+      });
     });
+  });
 });
--- spec/suites/esri-leafletSpec.js
+++ spec/suites/esri-leafletSpec.js
@@ -1,70 +1,80 @@
 /* globals L describe it expect*/
 
 describe('L.esri', function () {
-    var map,
-        url = 'http://services.arcgisonline.com/ArcGIS/rest/services/Demographics',
-        extent = {
-            xmin: 10,
-            xmax: 20,
-            ymin: 10,
-            ymax: 20,
-            spatialReference: { wkid: 4326 }
-        };
-    beforeEach(function () {
-        map = L.map(document.createElement('div'));
-        map.setView([37.75, -122.45], 12);
-    });
+  var sandbox,
+      url = 'http://services.arcgisonline.com/ArcGIS/rest/services/Demographics',
+      extent = {
+        xmin: 10,
+        xmax: 20,
+        ymin: 10,
+        ymax: 20,
+        spatialReference: { wkid: 4326 }
+      };
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+  });
+  afterEach(function () {
+    sandbox.restore();
+  });
 
-    describe('#get', function () {
-        describe('when the get method is used', function () {
-            it('will append script tags to document', function () {
-                document.body.appendChild = sinon.spy();
-                L.esri.get(url, { 'name': 'test' }, function () {
-                });
-                expect(document.body.appendChild.calledOnce).to.be.ok();
-            });
+  describe('#get', function () {
+    describe('when the get method is used', function () {
+      it('will append script tags to document', function () {
+        var appendSpy = sandbox.spy(document.body, 'appendChild');
+        L.esri.get(url, { 'name': 'test' }, function () {
         });
+        expect(appendSpy.calledOnce).to.be.ok();
+      });
     });
+  });
 
-    describe('#Util', function () {
-        describe('when using #indexOf', function () {
-            it('will find correct index of object in array', function () {
-                var first = { name: '1st' },
-                    second = { name: '2nd' },
-                    third = { name: '3rd' },
-                    arr = [ first, second, third ];
-                expect(L.esri.Util.indexOf(arr, first)).to.eql(0);
-                expect(L.esri.Util.indexOf(arr, third)).to.eql(2);
-                expect(L.esri.Util.indexOf(arr, first, 1)).to.eql(-1);
-                expect(L.esri.Util.indexOf(arr, {})).to.eql(-1);
-                expect(L.esri.Util.indexOf(arr, 1)).to.eql(-1);
-                expect(L.esri.Util.indexOf(arr, null)).to.eql(-1);
-            });
-        });
-        describe('when using #extentToBounds', function () {
-            it('will create Leaflet compatible bounds', function () {
-                expect(L.esri.Util.extentToBounds(extent).isValid()).to.be.ok();
-            });
-        });
-        describe('when using #boundsToExtent', function () {
-            xit('will create an extent from Leaflet bounds', function () {
-                var bounds = L.esri.Util.extentToBounds(extent),
-                    testExtent = L.esri.Util.boundsToExtent(bounds);
-                expect(testExtent.xmin).to.eql(extent.xmin);
-                expect(testExtent.xmax).to.eql(extent.xmax);
-                expect(testExtent.ymin).to.eql(extent.ymin);
-                expect(testExtent.ymax).to.eql(extent.ymax);
-            });
-        });
-        describe('when using #boundsToEnvelope', function () {
-            it('will create an envelope using L.esri.Util.boundsToExtent', function () {
-                var bounds = { name: 'test' };
-                L.esri.Util.boundsToExtent = sinon.stub().returns(extent);
-                var envelope = L.esri.Util.boundsToEnvelope(bounds);
-                expect(L.esri.Util.boundsToExtent.calledWith(bounds)).to.be.ok();
-                expect(envelope.x).to.eql(extent.xmin);
-                expect(envelope.y).to.eql(extent.ymin);
-            });
-        });
+  describe('#Util', function () {
+    describe('when using #indexOf', function () {
+      it('will find correct index of object in array', function () {
+        var first = { name: '1st' },
+          second = { name: '2nd' },
+          third = { name: '3rd' },
+          arr = [ first, second, third ];
+        expect(L.esri.Util.indexOf(arr, first)).to.eql(0);
+        expect(L.esri.Util.indexOf(arr, second)).to.eql(1);
+        expect(L.esri.Util.indexOf(arr, third)).to.eql(2);
+        expect(L.esri.Util.indexOf(arr, first, 1)).to.eql(-1);
+        expect(L.esri.Util.indexOf(arr, {})).to.eql(-1);
+        expect(L.esri.Util.indexOf(arr, 1)).to.eql(-1);
+        expect(L.esri.Util.indexOf(arr, '')).to.eql(-1);
+        expect(L.esri.Util.indexOf(arr, null)).to.eql(-1);
+        expect(L.esri.Util.indexOf(arr, undefined)).to.eql(-1);
+      });
     });
+    describe('when using #extentToBounds', function () {
+      it('will create Leaflet compatible bounds', function () {
+        var bounds = L.esri.Util.extentToBounds(extent);
+        expect(bounds.isValid()).to.be.ok();
+        expect(bounds.getSouthWest().lng).to.eql(extent.xmin);
+        expect(bounds.getSouthWest().lat).to.eql(extent.ymin);
+        expect(bounds.getNorthEast().lng).to.eql(extent.xmax);
+        expect(bounds.getNorthEast().lat).to.eql(extent.ymax);
+      });
+    });
+    describe('when using #boundsToExtent', function () {
+      it('will create an extent from Leaflet bounds', function () {
+        var bounds = L.esri.Util.extentToBounds(extent),
+            testExtent = L.esri.Util.boundsToExtent(bounds);
+        expect(testExtent.xmin).to.eql(extent.xmin);
+        expect(testExtent.xmax).to.eql(extent.xmax);
+        expect(testExtent.ymin).to.eql(extent.ymin);
+        expect(testExtent.ymax).to.eql(extent.ymax);
+      });
+    });
+    describe('when using #boundsToEnvelope', function () {
+      it('will create an envelope using L.esri.Util.boundsToExtent', function () {
+        var bounds = { name: 'test' },
+          boundsToExtentStub = sandbox.stub(L.esri.Util, 'boundsToExtent').returns(extent);
+        var envelope = L.esri.Util.boundsToEnvelope(bounds);
+        expect(boundsToExtentStub.calledWith(bounds)).to.be.ok();
+        expect(envelope.x).to.eql(extent.xmin);
+        expect(envelope.y).to.eql(extent.ymin);
+      });
+    });
+  });
 });
