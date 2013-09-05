f42b24603bc21a2afb9287e3aadc73348c85d668
Patrick Arlt <patrick.arlt@gmail.com>
2013-09-05 13:53:18 -0700

    fix #129, add connect server for debugging, add bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,6 +100,15 @@ module.exports = function(grunt) {
           dir : './coverage/'
         }
       }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.',
+          keepalive: true
+        }
+      }
     }
   });
 
@@ -111,6 +120,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-karma');
+
 
 };
