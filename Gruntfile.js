6feae110f3e2c9cef59c627f5312e0a1d1935f72
Ilya Radchenko <ilya@burstcreations.com>
2014-06-11 11:41:08 -0400

    quotes and jshint moved to rc file

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,23 +5,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef:  true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          XMLHttpRequest: true,
-          module: true,
-          L: true,
-          console: true
-        }
+        jshintrc: '.jshintrc'
       },
       all: {
         src: [
@@ -58,11 +42,11 @@ module.exports = function(grunt) {
         dest: 'dist/extras/esri-basemaps-src.js'
       },
       cluster: {
-        src: ["src/Layers/ClusteredFeatureLayer.js"],
+        src: ['src/Layers/ClusteredFeatureLayer.js'],
         dest: 'dist/extras/clustered-feature-layer-src.js'
       },
       heat: {
-        src: ["src/Layers/HeatMapFeatureLayer.js"],
+        src: ['src/Layers/HeatMapFeatureLayer.js'],
         dest: 'dist/extras/heatmap-feature-layer-src.js'
       }
     },
@@ -78,7 +62,7 @@ module.exports = function(grunt) {
       dist: {
         files: {
           'dist/esri-leaflet.js': [
-            "dist/esri-leaflet-src.js"
+            'dist/esri-leaflet-src.js'
           ],
           'dist/extras/esri-basemaps.js': [
             'dist/extras/esri-basemaps-src.js'
@@ -130,8 +114,8 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('default', ['jshint', "test"]);
-  grunt.registerTask('build', ['jshint', "karma:coverage",'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('build', ['jshint', 'karma:coverage','concat', 'uglify']);
   grunt.registerTask('test', ['karma:single']);
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
