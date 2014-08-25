deaeb14fec3645f988986187cd0d07a3baeaf322
Brendan Collins <bcollins@blueraster.com>
2014-08-25 11:29:09 -0400

    fixed missing semicolon

--- src/Layers/FeatureLayer/FeatureManager.js
+++ src/Layers/FeatureLayer/FeatureManager.js
@@ -92,7 +92,7 @@
         this._activeRequests--;
 
         if(!error && featureCollection.features.length){
-          this.setFieldAliases(response.fieldAliases)
+          this.setFieldAliases(response.fieldAliases);
           this._addFeatures(featureCollection.features, coords);
         }
 
