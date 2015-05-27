e30eeaa004fc25809155d9098cebdea0eb2c2baa
Patrick Arlt <patrick.arlt@gmail.com>
2015-05-27 08:46:25 -0700

    automatically remap tile levels if they are not in standard mercator

--- src/Layers/TiledMapLayer.js
+++ src/Layers/TiledMapLayer.js
@@ -1,4 +1,32 @@
 EsriLeaflet.Layers.TiledMapLayer = L.TileLayer.extend({
+  statics: {
+    MercatorZoomLevels: {
+      '0':156543.03392799999,
+      '1':78271.516963999893,
+      '2':39135.758482000099,
+      '3':19567.879240999901,
+      '4':9783.9396204999593,
+      '5':4891.9698102499797,
+      '6':2445.9849051249898,
+      '7':1222.9924525624899,
+      '8':611.49622628138002,
+      '9':305.74811314055802,
+      '10':152.874056570411,
+      '11':76.437028285073197,
+      '12':38.218514142536598,
+      '13':19.109257071268299,
+      '14':9.5546285356341496,
+      '15':4.7773142679493699,
+      '16':2.38865713397468,
+      '17':1.1943285668550501,
+      '18':0.59716428355981699,
+      '19':0.29858214164761698,
+      '20':0.14929107082381,
+      '21':0.07464553541191,
+      '22':0.0373227677059525,
+      '23':0.0186613838529763
+    }
+  },
   initialize: function(url, options){
     options = options || {};
     options.url = EsriLeaflet.Util.cleanUrl(url);
@@ -22,6 +50,42 @@ EsriLeaflet.Layers.TiledMapLayer = L.TileLayer.extend({
 
     // init layer by calling TileLayers initialize method
     L.TileLayer.prototype.initialize.call(this, this.tileUrl, options);
+  },
+
+  getTileUrl: function (tilePoint) {
+    return L.Util.template(this.tileUrl, L.extend({
+      s: this._getSubdomain(tilePoint),
+      z: this._lodMapper[tilePoint.z],
+      x: tilePoint.x,
+      y: tilePoint.y
+    }, this.options));
+  },
+
+  onAdd: function(map){
+    if (!this._lodMapper) {
+      this._lodMapper = {};
+      this.metadata(function(error, metadata){
+        //create the zoom level data
+        var arcgisLODs = metadata.tileInfo.lods;
+        var correctResolutions = EsriLeaflet.Layers.TiledMapLayer.MercatorZoomLevels;
+
+        for(var i = 0; i < arcgisLODs.length; i++) {
+            var arcgisLOD = arcgisLODs[i];
+            for(var ci in correctResolutions) {
+                var correctRes = correctResolutions[ci];
+
+                if(this._withinPercentage(arcgisLOD.resolution, correctRes, 0.1)) {
+                    this._lodMapper[ci] = arcgisLOD.level;
+                    break;
+                }
+            }
+        }
+
+        L.TileLayer.prototype.onAdd.call(this, map);
+      }, this);
+    } else {
+      L.TileLayer.prototype.onAdd.call(this, map);
+    }
   },
 
   metadata: function(callback, context){
@@ -49,6 +113,11 @@ EsriLeaflet.Layers.TiledMapLayer = L.TileLayer.extend({
       target: this
     }, e);
     this.fire(e.type, e);
+  },
+
+  _withinPercentage: function (a, b, percentage) {
+    var diff = Math.abs((a/b) - 1);
+    return diff < percentage;
   }
 });
 
