0a4fd9d67251ee878ad0470d7e359f588cf82b0e
Patrick Arlt <patrick.arlt@gmail.com>
2015-07-24 11:07:18 -0700

    fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -20,6 +20,9 @@ git add $FILES -f
 # commit changes with a versioned commit message
 git commit -m "build $VERSION"
 
+# push commit so it exists on GitHub when we run gh-release
+git push upstream gh-release
+
 # create a ZIP archive of the dist files
 zip -r dist/$NAME-v$VERSION.zip $FILES
 
