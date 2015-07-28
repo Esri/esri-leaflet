6bacfb51247f29d65772f623ac4beaf473f0e9a4
Patrick Arlt <patrick.arlt@gmail.com>
2015-07-28 09:04:42 -0700

    amd build

--- scripts/build.js
+++ scripts/build.js
@@ -3,13 +3,10 @@
 var path = require('path');
 var fs = require('fs');
 var rollup = require('rollup').rollup;
-var UglifyJS = require('uglify-js');
+var minifyer = require('./minify.js');
 var pkg = require('../package.json');
-var entryFile = 'src/EsriLeaflet.js';
 
-var copyright = '/* ' + pkg.name + ' - v' + pkg.version + ' - ' + new Date().toString() + '\n' +
-                ' * Copyright (c) ' + new Date().getFullYear() + ' Environmental Systems Research Institute, Inc.\n' +
-                ' * ' + pkg.license + ' */';
+var entryFile = 'src/EsriLeaflet.js';
 
 rollup({
   entry: path.resolve(entryFile),
@@ -22,24 +19,10 @@ rollup({
     moduleName: 'L.esri'
   });
 
-  var sourceMap = UglifyJS.SourceMap({
-    file: pkg.name + '.js',
-    root: process.cwd(),
-    orig: JSON.parse(transpiled.map)
-  });
+  var compressed = minifyer(transpiled.code, JSON.parse(transpiled.map));
 
-  var stream = UglifyJS.OutputStream({
-    preamble: copyright,
-    source_map: sourceMap
-  });
-
-  UglifyJS.parse(transpiled.code).print(stream);
-
-  var code = stream.toString();
-  var map = sourceMap.toString().replace(new RegExp(path.join(process.cwd(), 'src'), 'g'), '../src');
-
-  fs.writeFileSync(path.join('dist', pkg.name + '.js'), code + '\n//# sourceMappingURL=./' + pkg.name + '.js.map');
-  fs.writeFileSync(path.join('dist', pkg.name + '.js.map'), map);
+  fs.writeFileSync(path.join('dist', pkg.name + '.js'), compressed.code + '\n//# sourceMappingURL=./' + pkg.name + '.js.map');
+  fs.writeFileSync(path.join('dist', pkg.name + '.js.map'), compressed.map);
   process.exit(0);
 }).catch(function (error) {
   console.log(error);
