e46dfe786cf9ee2f15ae0e19e3e24896eda3b355
Alex Patow <alexpatow@users.noreply.github.com>
2016-08-01 15:39:27 -0400

    Fixes Issue 807 by adding token to url received from ArcGIS Server (#812)
    * Fixes Issue 807 by adding token to url received from ArcGIS Server
    
    * Removes trailing whitespace

--- src/Layers/ImageMapLayer.js
+++ src/Layers/ImageMapLayer.js
@@ -192,6 +192,9 @@ export var ImageMapLayer = RasterLayer.extend({
     if (this.options.f === 'json') {
       this.service.request('exportImage', params, function (error, response) {
         if (error) { return; } // we really can't do anything here but authenticate or requesterror will fire
+        if (this.options.token) {
+          response.href += ('?token=' + this.options.token);
+        }
         this._renderImage(response.href, bounds);
       }, this);
     } else {
