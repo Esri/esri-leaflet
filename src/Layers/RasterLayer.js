e281a3d8ff6ea823993af30df21b6aef3423bc9c
john gravois <jgravois@esri.com>
2016-12-16 13:08:23 -0800

    only remove existing images

--- src/Layers/RasterLayer.js
+++ src/Layers/RasterLayer.js
@@ -229,7 +229,9 @@ export var RasterLayer = L.Layer.extend({
     }
 
     if (zoom > this.options.maxZoom || zoom < this.options.minZoom) {
-      this._currentImage._map.removeLayer(this._currentImage);
+      if (this._currentImage) {
+        this._currentImage._map.removeLayer(this._currentImage);
+      }
       return;
     }
 
