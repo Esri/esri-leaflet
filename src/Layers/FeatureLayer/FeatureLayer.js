e12bbbd68cc1ca7c7e3277258dc99c4f7a65c999
john gravois <jgravois@esri.com>
2017-01-05 09:17:18 -0800

    make sure addfeature is only called on readd (#893)
    * make sure addfeature is only called once
    
    * create OR add

--- src/Layers/FeatureLayer/FeatureLayer.js
+++ src/Layers/FeatureLayer/FeatureLayer.js
@@ -120,9 +120,6 @@ export var FeatureLayer = FeatureManager.extend({
 
         // add the layer if the current zoom level is inside the range defined for the layer, it is within the current time bounds or our layer is not time enabled
         if (this._visibleZoom() && (!this.options.timeField || (this.options.timeField && this._featureWithinTimeRange(geojson)))) {
-          this.fire('addfeature', {
-            feature: newLayer.feature
-          }, true);
           this._map.addLayer(newLayer);
         }
       }
@@ -133,9 +130,6 @@ export var FeatureLayer = FeatureManager.extend({
     for (var i = ids.length - 1; i >= 0; i--) {
       var layer = this._layers[ids[i]];
       if (layer) {
-        this.fire('addfeature', {
-          feature: layer.feature
-        }, true);
         this._map.addLayer(layer);
       }
     }
