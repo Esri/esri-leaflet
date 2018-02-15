2f967a019949ea45c44ae0688bc1a1465e8b4ac0
john gravois <jgravois@esri.com>
2018-02-15 15:47:33 -0800

    bump rollup

--- profiles/production.js
+++ profiles/production.js
@@ -1,8 +1,8 @@
 import uglify from 'rollup-plugin-uglify';
 import config from './base.js';
 
-config.dest = 'dist/esri-leaflet.js';
-config.sourceMap = 'dist/esri-leaflet.js.map';
+config.output.file = 'dist/esri-leaflet.js';
+config.output.sourcemap = 'dist/esri-leaflet.js.map';
 
 // use a Regex to preserve copyright text
 config.plugins.push(uglify({ output: {comments: /Institute, Inc/} }));
