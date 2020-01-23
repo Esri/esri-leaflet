4b84d4c01d10d68fcf05893b4ebf2b7e8f5ae96f
jwasilgeo <jwasilkowski@esri.com>
2020-01-23 09:35:27 -0600

    removed options.f conditional override in DynamicMapLayer `initialize`

--- src/Layers/DynamicMapLayer.js
+++ src/Layers/DynamicMapLayer.js
@@ -20,10 +20,6 @@ export var DynamicMapLayer = RasterLayer.extend({
     this.service = mapService(options);
     this.service.addEventParent(this);
 
-    if ((options.proxy || options.token) && options.f !== 'json') {
-      options.f = 'json';
-    }
-
     Util.setOptions(this, options);
   },
 
