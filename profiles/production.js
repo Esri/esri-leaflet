5d6d0bdd8191e1c514b18cc0c0bcc90ae5046c92
pavlenko <destus90@gmail.com>
2020-05-18 17:30:29 +0500

    transform bundled code with babel for production profile

--- profiles/production.js
+++ profiles/production.js
@@ -1,9 +1,12 @@
 import uglify from 'rollup-plugin-uglify';
+import babel from '@rollup/plugin-babel';
 import config from './base.js';
 
 config.output.file = 'dist/esri-leaflet.js';
 
 // use a Regex to preserve copyright text
 config.plugins.push(uglify({ output: {comments: /Institute, Inc/} }));
+// transform with babel
+config.plugins.push(babel({ babelHelpers: 'bundled' }));
 
 export default config;
