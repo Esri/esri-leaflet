1b0d40dc6ba5de06286af3c8433622285240fe12
Gavin Rehkemper <gavin@gavinr.com>
2020-10-04 22:46:26 -0500

    use rollup-plugin-terser
    instead of rollup-plugin-uglify

--- profiles/production.js
+++ profiles/production.js
@@ -1,9 +1,9 @@
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 import config from './base.js';
 
 config.output.file = 'dist/esri-leaflet.js';
 
 // use a Regex to preserve copyright text
-config.plugins.push(uglify({ output: {comments: /Institute, Inc/} }));
+config.plugins.push(terser());
 
 export default config;
