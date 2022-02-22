d866bf9e31d13a9b89bb787110895e1ee1bf7b47
Gavin Rehkemper <gavin@gavinr.com>
2022-02-22 17:29:24 -0600

    switch to rollup-plugin-terser

--- profiles/production.js
+++ profiles/production.js
@@ -1,9 +1,9 @@
-import { uglify } from 'rollup-plugin-uglify';
-import config from './base.js';
+import { terser } from "rollup-plugin-terser";
+import config from "./base.js";
 
-config.output.file = 'dist/esri-leaflet.js';
+config.output.file = "dist/esri-leaflet.js";
 
 // use a Regex to preserve copyright text
-config.plugins.push(uglify({ output: {comments: /Institute, Inc/} }));
+config.plugins.push(terser({ format: { comments: /Institute, Inc/ } }));
 
 export default config;
