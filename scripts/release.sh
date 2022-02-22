564802be9b31330ada6ea545d92a9ae058ff4e4e
Gavin Rehkemper <gavin@gavinr.com>
2022-02-22 17:06:46 -0600

    save to dist

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,7 @@ npm test || exit 1
 
 # Integrity string and save to siteData.json
 JS_INTEGRITY=$(cat dist/esri-leaflet.js | openssl dgst -sha512 -binary | openssl base64 -A)
-echo "{\"name\": \"esri-leaflet\",\"version\": \"$VERSION\",\"lib\": {\"path\": \"dist/esri-leaflet.js\",\"integrity\": \"sha512-$JS_INTEGRITY\"}}" > siteData.json
+echo "{\"name\": \"esri-leaflet\",\"version\": \"$VERSION\",\"lib\": {\"path\": \"dist/esri-leaflet.js\",\"integrity\": \"sha512-$JS_INTEGRITY\"}}" > dist/siteData.json
 
 # checkout temp branch for release
 git checkout -b gh-release
