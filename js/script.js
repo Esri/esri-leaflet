e49ee6061d45f3c34b7575889b63db600f16b386
patrickarlt <patrickarlt@users.noreply.github.com>
2022-05-10 18:45:30 +0000

    deploy: Esri/esri-leaflet-doc@078a64f93e11c124a8ca31ac3597b4ef7fe9cc95

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-L.map('background-map', {
-  center: [37.739, -117.986],
-  zoom: 10,
-  scrollWheelZoom: false,
-  touchZoom: true,
-  zoomControl: false,
-  tap: false,
-  attributionControl: false,
-  layers: [L.esri.basemapLayer('Topographic')]
-});
-
-var map;
-
-if (map) {
-  map.scrollWheelZoom.disable();
-  map.once('click', accidentalScroll, map);
-}
-
-function accidentalScroll () {
-  map.scrollWheelZoom.enable();
-}
 
 // Automatically generate links on the heading elements on a page (if they have an `id` node attribute)
 var pageHeadings = document.querySelectorAll('h2[id], h3[id], h4[id], h5[id], h6[id]');
