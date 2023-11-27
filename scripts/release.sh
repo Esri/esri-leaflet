288e807206d4be7d2f91c73f1429f64a95b2e6fd
Gavin Rehkemper <grehkemper@esri.com>
2023-11-27 08:48:01 -0600

    fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -21,7 +21,7 @@ git add dist -f
 git commit -m "build $VERSION"
 
 # push commit so it exists on GitHub when we run gh-release
-git push https://github.com/Esri/esri-leaflet gh-release
+git push git@github.com:Esri/esri-leaflet.git gh-release
 
 # create a ZIP archive of the dist files
 zip -r $NAME-v$VERSION.zip dist
@@ -35,4 +35,4 @@ npm publish
 # checkout master and delete release branch locally and on GitHub
 git checkout master
 git branch -D gh-release
-git push https://github.com/Esri/esri-leaflet :gh-release
+git push git@github.com:Esri/esri-leaflet.git :gh-release
