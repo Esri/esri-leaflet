45534dd9ce1903887b651207433741b7fc879210
Gavin Rehkemper <grehkemper@esri.com>
2025-04-11 15:57:52 -0500

    fix config order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,56 +7,56 @@ import eslintConfigPrettier from "eslint-config-prettier/flat";
 import config from "eslint-config-mourner";
 
 export default defineConfig([
-  config,
-  eslintConfigPrettier,
-  {
-    files: ["*.js", "*.cjs"],
-  },
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-    },
-  },
-  {
-    ignores: ["dist", "coverage"],
-  },
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      "dot-notation": "off",
-      "consistent-return": "off",
-      curly: "error",
-      "no-unused-expressions": ["error", { allowShortCircuit: true }],
-      "no-unused-vars": ["error", { caughtErrors: "none" }],
+	config,
+	{
+		files: ["*.js", "*.cjs"],
+	},
+	{
+		languageOptions: {
+			ecmaVersion: "latest",
+		},
+	},
+	{
+		ignores: ["dist", "coverage"],
+	},
+	{
+		plugins: {
+			import: importPlugin,
+		},
+		rules: {
+			"dot-notation": "off",
+			"consistent-return": "off",
+			curly: "error",
+			"no-unused-expressions": ["error", { allowShortCircuit: true }],
+			"no-unused-vars": ["error", { caughtErrors: "none" }],
 
-      "import/extensions": ["error", "ignorePackages"],
+			"import/extensions": ["error", "ignorePackages"],
 
-      "@stylistic/js/indent": [
-        "error",
-        "tab",
-        { VariableDeclarator: 0, flatTernaryExpressions: true },
-      ],
-      "@stylistic/js/no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
-      "@stylistic/js/key-spacing": "off",
-      "@stylistic/js/linebreak-style": ["off", "unix"],
-      "@stylistic/js/spaced-comment": "error",
+			"@stylistic/js/indent": [
+				"error",
+				"tab",
+				{ VariableDeclarator: 0, flatTernaryExpressions: true },
+			],
+			"@stylistic/js/no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+			"@stylistic/js/key-spacing": "off",
+			"@stylistic/js/linebreak-style": ["off", "unix"],
+			"@stylistic/js/spaced-comment": "error",
 
-      // TODO: Re-enable the rules below and fix the linting issues.
-      "no-invalid-this": "off",
-      "prefer-object-has-own": "error",
-      "prefer-spread": "off",
-      "no-new": "off",
-    },
-  },
-  {
-    files: ["spec/**"],
-    languageOptions: {
-      globals: { ...globals.mocha, expect: "readable", L: "writeable" },
-    },
-    rules: {
-      "no-unused-expressions": "off",
-    },
-  },
+			// TODO: Re-enable the rules below and fix the linting issues.
+			"no-invalid-this": "off",
+			"prefer-object-has-own": "error",
+			"prefer-spread": "off",
+			"no-new": "off",
+		},
+	},
+	{
+		files: ["spec/**"],
+		languageOptions: {
+			globals: { ...globals.mocha, sinon: "readable", expect: "readable", L: "writeable" },
+		},
+		rules: {
+			"no-unused-expressions": "off",
+		},
+	},
+	eslintConfigPrettier
 ]);
